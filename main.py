dfd6f0ad8b22d2380771f3a5e64e9dc84242f60e
h3rl <halvard.erla@gmail.com>
2022-12-28 21:46:24 +0100

    cleaned up threading

--- main.py
+++ main.py
@@ -1,97 +1,84 @@
-
-import threading
 from gui import ConsoleWindow
 
+import threading
+import time
+import serial
+import serial.tools
 
-def main():
-    #while True:
-    print("Port is open on ",port)
-
-    # send AT command
-    b = str("AT\r\n").encode()
-    print("Writing bytes:")
-    print(b)
-    ser.write_timeout = 1
-    try:
-        ser.write(b)
-    except serial.SerialTimeoutException:
-        print("Write timeout")
-        ser.close()
-        #continue
-    data = ser.readall()
-    print(data)
-    ser.close()
-
-def TxThread(ser):
-    """Thread to write to serial port"""
-    while True:
-        command = input("Enter command: ")
-        # parse local commands first
-        if command == "help":
-            print("Commands: ping, exit")
-            continue
-
-        while not ser.writeable():
-            time.sleep(1)
-        if command == "ping":
-            ser.write(b"ping\r\n")
-        elif command == "exit":
-            print("Exiting")
-            ser.close()
-            if ser.closed:
-                print("Serial port closed successfully!")
-            else:
-                print("Failed to close serial port")
-            sys.exit(0)
-        else:
-            print("Invalid command, help for list of commands")
-        
-
-def RxThread(ser):
-    """Thread to read from serial port"""
-    while True:
-        if ser.readable():
-            data = ser.readall()
-            print(data)
+import util
 
 # globals
 app = None
 should_exit = False
 ser = None
 recieve_thread = None
-send_thread = None
-app_thread = None
+ 
+def RxThread():
+    """Thread to read from serial port"""
+    while not should_exit:
+        if ser and ser.readable():
+            try:
+                data = ser.readall()
+            except util.SerialTimeoutException:
+                app.console_log("Read Timeout","error")
+            
+            if data:
+                try:
+                    app.console_log(data.decode())
+                except:
+                    app.console_log(data)
+        time.sleep(0.1)
 
 def on_user_command(command):
     # parse local commands first
     if command == "help":
-        print("Commands: ping, exit")
+        app.console_log("Commands: ping, exit")
         return
     elif command == "exit":
         if ser:
             ser.close()
             if ser.closed:
-                print("Serial port closed successfully!")
+                app.console_log("Serial port closed successfully!")
             else:
-                print("Failed to close serial port")
+                app.console_log("Failed to close serial port")
         should_exit = True
-
-    if command == "ping":
+        app.quit()
+    elif command == "ping":
         send_arduino_command("ping")
-        
-    # parse and send to arduino
-    # send to arduino
-    pass
+    else:
+        app.console_log("Invalid command, help for list of commands")
+        return
 
 def send_arduino_command(command):
-    pass
+    if not ser:
+        app.console_log("No serial port open")
+        return
+
+    tick = 0
+    while not ser.writable():
+        time.sleep(0.1)
+        tick += 1
+        if tick > 50:# 5 seconds
+            app.console_log("Timed out, serial port not writable","error")
+            return
+
+    b = str(command + app.get_line_ending()).encode()
+    ser.write_timeout = 1
+    try:
+        ser.write(b)
+    except util.SerialTimeoutException:
+        app.console_log("Write Timeout","error")
+    return
 
 if __name__ == "__main__":
     app = ConsoleWindow()
-    app.on_command(on_user_command, ending=True)
+    app.on_command(on_user_command)
     # create threads
-    app_thread = threading.Thread(target=app.mainloop)
-    app_thread.start()
-    main()
-    app_thread.join()
+    recieve_thread = threading.Thread(target=RxThread)
+    recieve_thread.start()
+    app.mainloop()
+    
+    # clean up
+    should_exit = True
+    recieve_thread.join()
     
