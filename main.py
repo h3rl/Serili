6931fa9fa2cb7e36f27abd3ce22236c414582d2e
h3rl <halvard.erla@gmail.com>
2022-12-28 23:30:46 +0100

    fix exit, add open and cls

--- main.py
+++ main.py
@@ -30,28 +30,72 @@ def RxThread():
         time.sleep(0.1)
 
 def on_user_command(command):
+    global ser
     # parse local commands first
+    command, *args = command.split(" ")
     if command == "help":
         app.console_log("Commands: ping, exit")
         return
     elif command == "exit":
-        if ser:
+        if ser and ser.is_open:
+            ser.close()
+        should_exit = True
+        app.quit()
+    elif command == "ping":
+        send_arduino_command("ping")
+    elif command == "close":
+        if ser and ser.is_open:
             ser.close()
             if ser.closed:
                 app.console_log("Serial port closed successfully!")
             else:
                 app.console_log("Failed to close serial port")
-        should_exit = True
-        app.quit()
-    elif command == "ping":
-        send_arduino_command("ping")
+        else:
+            app.console_log("No serial port open")
+    elif command in ["open", "connect"]:
+        if len(args) == 0:
+            app.console_log("Invalid arguments","warning")
+            app.console_log("Usage: open <port> <baudrate>", "hint")
+            app.console_log("hint: use listport to list available ports", "hint")
+            return
+        
+        port, baudrate = None, None        
+        try:
+            port = args[0]
+            if len(args) > 1:
+                baudrate = int(args[1])
+            else:
+                app.console_log("Using default baudrate: 9600","hint")
+                baudrate = 9600
+        except ValueError:
+            app.console_log("Invalid arguments","warning")
+            app.console_log("Usage: open <port> <baudrate>", "hint")
+            app.console_log("hint: use listport to list available ports", "hint")
+            return
+        if ser and ser.is_open:
+            app.console_log("Some serial port is already open","warning")
+            return
+        try:
+            ser = serial.Serial(port, baudrate, timeout=1)
+        except serial.SerialException as e:
+            app.console_log(f"Failed to open serial port: {port}","error")
+            return
+        if ser.is_open:
+            app.console_log(f"{port} opened", "success")
+        else:
+            app.console_log(f"Failed to open serial port: {port}","error")
+            return
+    elif command == "listport":
+        pass
+    elif command == "cls":
+        app.clear_console()
     else:
-        app.console_log("Invalid command, help for list of commands")
+        app.console_log("Invalid command, help for list of commands","warning")
         return
 
 def send_arduino_command(command):
     if not ser:
-        app.console_log("No serial port open")
+        app.console_log("No serial port open","error")
         return
 
     tick = 0
